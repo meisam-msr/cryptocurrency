ffc44c0c163cce81c4b708f5173fad9a7618d1b9
meisam <maghsoodiradmeisam12@gmail.com>
2022-07-12 00:03:45 +0430

    add template and routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import Navbar from "./components/Navbar";
+import "App.css";
+import { Layout, Space, Typography } from "antd";
+import { Link, Route, Routes } from "react-router-dom";
+import Navbar from "components/Navbar/Navbar";
+import HomePage from "components/HomePage";
+import Exchanges from "components/Exchanges";
+import Cryptocurrencies from "components/Cryptocurrencies";
+import CryptoDetails from "components/CryptoDetails";
+import News from "components/News";
 
 function App() {
   return (
@@ -6,8 +14,33 @@ function App() {
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
-      <div className="footer"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:cryptoId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 }
