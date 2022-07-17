a0641a94185516040699d3c90267ce1e9971f6b6
meisam <maghsoodiradmeisam12@gmail.com>
2022-07-17 17:45:09 +0430

    add rtk,rtk query,.env

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import store from "app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
