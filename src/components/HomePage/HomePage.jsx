9d9da9257ea3ff37e9d4ae9cfcb08ff5b771be15
meisam <maghsoodiradmeisam12@gmail.com>
2022-07-17 19:47:08 +0430

    get data and show them in homePage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,19 @@
 import { Col, Row, Statistic, Typography } from "antd";
 import React from "react";
 import styles from "./homePage.module.css";
+import { useGetCryptosQuery } from "services/cryptoApi";
+import { longNumToStr } from "utils/longNumToStr";
+import { Link } from "react-router-dom";
+import Cryptocurrencies from "components/Cryptocurrencies/Cryptocurrencies";
+import News from "components/News";
 const { Title } = Typography;
 
 const HomePage = () => {
+  const { data, isFetching } = useGetCryptosQuery(10);
+  const globalStats = data?.data?.stats;
+
+  if (isFetching) return "Loading...";
+
   return (
     <>
       <Title level={2} className={styles.heading}>
@@ -11,21 +21,54 @@ const HomePage = () => {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value="5" />
+          <Statistic
+            title="Total Cryptocurrencies"
+            value={longNumToStr(globalStats.total)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value="5" />
+          <Statistic
+            title="Total Exchanges"
+            value={longNumToStr(globalStats.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value="5" />
+          <Statistic
+            title="Total Market Cap"
+            value={longNumToStr(globalStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value="5" />
+          <Statistic
+            title="Total 24h Volume"
+            value={longNumToStr(globalStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market" value="5" />
+          <Statistic
+            title="Total Market"
+            value={longNumToStr(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
+      <div className={styles.homeHeadingContainer}>
+        <Title level={2} className={styles.homeTitle}>
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className={styles.showMore}>
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className={styles.homeHeadingContainer}>
+        <Title level={2} className={styles.homeTitle}>
+          Latest Crypto News
+        </Title>
+        <Title level={3} className={styles.showMore}>
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 };
